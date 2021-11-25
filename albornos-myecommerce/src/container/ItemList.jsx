a72bc7200b4b37d8ac5eb3fe25152a1a725fe0b1
AlbornosAgustín <amalbornos@gmail.com>
2021-11-25 00:21:29 -0300

    Desafio 3

---
+++ albornos-myecommerce/src/container/ItemList.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import item from './item'
+
+const itemList = (prop) => {
+   
+    return (
+        <div>
+           Remeras.map( prop ={<item/>}  ) 
+        </div>
+    )
+}
+
+export default itemList
--- albornos-myecommerce/src/container/ItemListContainer.jsx
+++ albornos-myecommerce/src/container/ItemListContainer.jsx
@@ -1,8 +1,32 @@
-import Item from "../container/item"
+import { useState,useEffect } from "react"
+import itemList from "./itemList"
+
 const ItemListContainer = () => {
+   
+    const [loading,setloading] = useState(true)
+    useEffect(() => {
+       getFetch
+       .then(data=>{
+           console.log('llamada Api')
+           setRemeras(data)
+       })
+       .catch(err=>console.log(err))
+       .finally(()=>setLoading(false))
+       return () => {
+           console.log('clean')
+       }
+   }, [])
+   
     return (
         <div>
-            <Item prod="Remera" price="$1500" imgUrl="img\Remera1.jpeg"/>
+            <div>
+             {loading ? <h1>Cargando..</h1>: <itemList/>
+             
+            
+            }
+
+            </div>
+            
         </div>
     )
 }
