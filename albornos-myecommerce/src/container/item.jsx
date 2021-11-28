fceadf419ce025a0afc4d81e76a6c9b975f73057
AlbornosAgustín <amalbornos@gmail.com>
2021-11-27 21:12:24 -0300

    Primer entrega de proyecto-Albornos

--- albornos-myecommerce/src/container/item.jsx
+++ albornos-myecommerce/src/container/item.jsx
@@ -1,10 +1,15 @@
+import './item.css';
 const item = ({prop}) => {
     return (
-        <div>
-          <h1>{prop.prod}</h1>  
-          <img src={prop.imgUrl} alt=""></img>
+      <div class="page-contentC">
+        <div class="product-conteiner">
+          <h1 >{prop.prod}</h1>  
+          <img  src={prop.imgUrl} alt=""></img>
           <h1>Precio:{prop.price}</h1>    
         </div>
+
+      </div>
+        
     )
 }
 
